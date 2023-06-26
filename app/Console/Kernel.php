bcf975e6cff78b412fad949e822eb7dd711e5215
weasel-v-dev <madara.avv@gmail.com>
2023-06-26 17:30:55 +0300

    Change directory to write logs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('importUsers:everyMinutes')->everyMinute()->appendOutputTo('scheduler.log');
+         $schedule->command('importUsers:everyMinutes')->everyMinute()->appendOutputTo('storage/logs/scheduler.log');
     }
 
     /**
